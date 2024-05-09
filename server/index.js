8db02f539266c76101b4634c7e3ed35931660e0b
Abdul kadar khan <95237480+Akprogramm@users.noreply.github.com>
2024-05-10 04:06:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 dotenv.config();    
 
 app.use(cors({
-    origin: 'http://localhost:5173' 
+    origin: '' 
 }));
 
 app.use(express.json({limit: '50mb'}));            
